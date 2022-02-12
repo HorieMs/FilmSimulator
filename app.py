b2be8158a766e6b7e12fc801d1d881b74f127b12
HorieMs <horie.ms10@gmail.com>
2022-02-12 20:44:12 +0900

    17th commit

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def get_nk_list():
 
 
     
-    nk_dirs=".\\data\\nk"
+    nk_dirs="data\\nk"
     files=os.listdir(nk_dirs)
     nk_files=[f for f in files if os.path.isfile(os.path.join(nk_dirs, f))]
  
